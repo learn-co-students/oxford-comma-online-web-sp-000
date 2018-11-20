1c0ed858b41ee24f9cb83806f5b2aa6378793bff
Cheng Vue <cvu3@ucsc.edu>
2018-11-20 10:11:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size > 2
+    str = ""
+    array.each do |ele|
+      ele_idx = array.index(ele)
+      last_ele_idx = array.size - 1
+      ele_idx == last_ele_idx ? str += "and #{ele}" : str += "#{ele}, "
+    end
+    return str
+  end
 end
