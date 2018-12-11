5a8bbe719324c89057d2aade7cca326b2d1ff8c9
Ariel <arielord@buffalo.edu>
2018-12-11 16:25:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.size < 2
+    array.join
+  elsif array.size == 2
+    "#{array[0]} and #{array[1]}"
+  else
+    "#{array[0...array.size-1].join(", ")}, and #{array[-1]}"
+  end
 end
