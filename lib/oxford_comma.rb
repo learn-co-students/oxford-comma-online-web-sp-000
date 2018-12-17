e9023e18b453085988c53140bf0c9e12e28aae54
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-12-17 20:57:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  last = array.last
+  last_len = last.length
+  if array.length == 1
+  string = array.join
+  elsif array.length == 2
+  string = "#{array[0]} and #{array[1]}"
+  else
+      temp_string = array.join(', ')
+      string_length = temp_string.length
+      reduce_last_item = string_length - last_len
+      temp = temp_string.slice(0,reduce_last_item)
+      string = "#{temp}and #{last}"
+  end
+return string
 end
