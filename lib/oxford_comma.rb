369f789c2fb655b2b9bf9e56e85c39f892a9c9fe
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-12-17 21:04:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,26 @@
+#def oxford_comma(array)
+#  last = array.last
+#  last_len = last.length
+#  if array.length == 1
+#  string = array.join
+#  elsif array.length == 2
+#  string = "#{array[0]} and #{array[1]}"
+#  else
+#      temp_string = array.join(', ')
+#      string_length = temp_string.length
+#      reduce_last_item = string_length - last_len
+#      temp = temp_string.slice(0,reduce_last_item)
+#      string = "#{temp}and #{last}"
+#  end
+#return string
+#end
+
+# way better
 def oxford_comma(array)
-  last = array.last
-  last_len = last.length
-  if array.length == 1
-  string = array.join
-  elsif array.length == 2
-  string = "#{array[0]} and #{array[1]}"
-  else
-      temp_string = array.join(', ')
-      string_length = temp_string.length
-      reduce_last_item = string_length - last_len
-      temp = temp_string.slice(0,reduce_last_item)
-      string = "#{temp}and #{last}"
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+    elsif 2 < array.length
+   array[-1].insert(0, "and ")
   end
-return string
+  array.join(", ")    
 end
