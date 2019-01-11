33b884b99fd387c765070fa007345683cad5c163
Alexandra Palermo <alexmpalermo@gmail.com>
2019-01-11 22:27:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
 
+
+if array.length == 2 
+  array.join(" and ")
+  
+  elsif array.length >= 3 
+  
+  array[0..-2].join(", ") + ", and " + array[-1]
+  
+  
+else
+array.join(",")
+end
 end
