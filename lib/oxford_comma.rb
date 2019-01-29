1064b9c39508ca38249f22378e06c6cf17eac507
Bry Cox <carl.cc@gmail.com>
2019-01-29 16:20:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+arr = []
+arr << array[-1]
+array.pop
+if array.count > 1
+  array.each {|x| x.insert(-1,",")}
+end
+array.insert(-1, "and") if array.count >= 1
+array.insert(-1, arr[0])
+array.join(" ")
 end
