099ebb6ef88083719e8099372c8ca1947d710f13
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-18 00:59:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
+require 'pry'
 def oxford_comma(array)
- array.join(" and ")
+    
+    if array.length == 1 
+      array.join 
+    elsif array.length == 2 
+     array.join(" and ")
+    elsif array.length == 3
+      array[-1].insert(0 , "and ")
+      array.join(", ")
+    elsif array.length> 3
+      array[-1].insert(0 , "and ")
+      array.join(", ")
+    end 
+    
 end
