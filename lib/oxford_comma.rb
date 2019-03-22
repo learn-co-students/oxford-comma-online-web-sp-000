c5bab88e809c3531c1f771f46ad335da7468a054
Yingqi Chen <chanwk18@hotmail.com>
2019-03-22 23:18:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  size=array.size
+  case size
+  when 1
+    return array[0]
+  when 2
+    return array.join("and")
+  when 3
+    return "#{array[0]},#{array[1]} and #{array[2]}
+  end
 
 end
