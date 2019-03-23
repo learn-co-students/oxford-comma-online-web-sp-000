71cf806f1794e8fff24a1c39e263100e6062743e
Yingqi Chen <chanwk18@hotmail.com>
2019-03-23 06:17:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,7 @@ if size==1
     return array[0]
   elsif size==2
     return array.join(" and ")
-  elsif size==3
-    return "#{array[0]}, #{array[1]}, and #{array[2]}"
-  else
+  elsif size>=3
     array[-1]="and #{array[-1]}"#change the value of last item to "and"+item and then join it
     array.join(", ")
   end
