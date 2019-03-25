ff9c157c5f21fe9da7031f1a02029caf20242633
Chinedu Arinze <c.s.arinze@gmail.com>
2019-03-25 20:08:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+def oxford_comma(arr)
 
+  if arr.size > 2
+    arr[-1] = "and #{arr[-1]}"
+    arr.join(", ")
+  elsif arr.size == 2
+    arr[-1] = " and #{arr[-1]}"
+    arr.join
+  else
+    arr.join
+  end
 end
