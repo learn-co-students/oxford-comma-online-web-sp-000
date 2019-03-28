709cd57ca46cc846fbb98cc327d55d9a87a291b6
Ashley Miller <asm2fe@virginia.edu>
2019-03-28 02:21:43 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+  temp=""
+  case array.size
+  when 1
+    return array.join
+  when 2
+    return array.join(" and ")
+  else
+    temp="and "
+    temp<<array.pop
+    array<<temp
+    array.join(", ")
+  end
 
 end
