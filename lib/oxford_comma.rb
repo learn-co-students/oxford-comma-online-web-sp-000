dcb60978d050fdb2d27532d12c775faffc8bcf47
Arthur Song <arth3rs0ng@gmail.com>
2019-04-04 01:17:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    last = array.pop
+    "#{array.join(", ")}, and #{last}"
+  end
 end
