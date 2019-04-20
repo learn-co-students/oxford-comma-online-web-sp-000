8430e038f1d075cda7e5eef54df8e1e2fed159de
renaynay <github email address>
2019-04-20 16:03:38 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length == 1
+    array.join
+  else
+    string = array.pop
+    new_string = array.join(", ")
+    new_string << ", and #{string}"
+    new_string
+  end
 end
