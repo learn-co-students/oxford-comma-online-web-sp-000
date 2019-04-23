be3ff6618fe8e5f811f4d9512d424049bb3646c1
Rick Rosetti <rick.rosetti@gmail.com>
2019-04-23 03:10:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    all_but_last = array[0..array.size-2].join(", ")
+    all_but_last + ", and " + array[array.size-1]
+  end
 
 end
