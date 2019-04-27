8938787f51519445badd7daae80d6bca8b0ee6b9
Anjana Nair <anju740@gmail.com>
2019-04-27 17:00:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,8 @@
 def oxford_comma(array)
-  array.each do |word|
-    if array.size >= 3
-      saved = array.pop
-      return array * ", " + ", and #{saved}"
-    else
-      return array.join(" and ")
-    end
+  if array.size >= 3
+    saved = array.pop
+    return array * ", " + ", and #{saved}"
+  else
+    return array.join(" and ")
   end
 end
