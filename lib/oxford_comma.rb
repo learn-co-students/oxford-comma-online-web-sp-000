dee70eac879e99bed9d6d4634f029a75e6fe3075
Anjana Nair <anju740@gmail.com>
2019-04-27 16:58:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  array.each do |word|
+    if array.size >= 3
+      saved = array.pop
+      return array * ", " + ", and #{saved}"
+    else
+      return array.join(" and ")
+    end
+  end
 end
