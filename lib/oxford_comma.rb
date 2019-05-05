152d8a3b38975f48cea837332a71e3b6e76a62e4
Abagail Hudson <abagailanne@gmail.com>
2019-05-05 04:00:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+ if array.length < 2
+   array.join
+ elsif array.length ==2
+   array.join(' and ')
+ else
+    temp = ", and " + array.pop
+    newString = array.join(", ") << temp
+  end
 end
