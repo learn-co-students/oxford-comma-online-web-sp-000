1d40d4679f73db5198746d8461c9e32e156cb601
elijahmm17 <elijahmm17@gmail.com>
2019-05-14 01:56:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1 
+    puts array[0]
+    return array[0]
+  elsif array.size == 2 
+    puts "#{array[0]} and #{array[1]}"
+    return "#{array[0]} and #{array[1]}"
+  else 
+    array.last.insert(0, "and ")
+    array.join(', ')
+  end
 end
