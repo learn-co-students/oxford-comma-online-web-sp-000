c7e27f50554e4598944713361c432545fa765177
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-24 21:28:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  if array.count == 1
+    return array.join
+  elsif array.count == 2 && array.count < 3
+    array.join(" and ")
+  else array.count == 3 || array.count > 3
+    #array.join(", ").insert(-2, "and")
+    array.insert(-2, "and").join(", ")
+  end
 end
