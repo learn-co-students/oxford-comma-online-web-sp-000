e53ac582aa904bc035745162d99f25ffe16951c9
Becky Phan <beckyphan@live.com>
2019-05-24 03:16:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3 
+    last_item = array[-1]
+    array[-1] = "and " + last_item
+    array.join(", ")
+  else
+    array[0]
+  end
 end
