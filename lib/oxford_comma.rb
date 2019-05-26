e8658fe2c58f3eb9444cc0fe1bce139c6bb3ac55
juan siu <juan.siu@gmail.com>
2019-05-26 20:31:43 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    if array.size == 2
+    array.join (" and ") 
+    
+    elsif  array.size < 2
+    array.join
+    
+    elsif array.size >= 3
+   message = array[0,array.size - 1]
+   message.join(", ") + ", and #{array.last}"
+    
+end
 end
