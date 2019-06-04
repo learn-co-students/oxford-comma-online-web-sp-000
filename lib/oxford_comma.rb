7cf969153ce478f61f84dfd4f88b9dff72a4ad47
Connor Cowan <ccowan003@comcast.net>
2019-06-04 22:04:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+#for this lab. set if conditions to perform certain conversions depending upon the length of our data
 
+def oxford_comma(array)
+  if array.length == 1
+    array.join('')
+  elsif array.length == 2
+    array.join(' and ')
+  elsif array.length >= 3
+      array[-1] = "and #{array[-1]}"        #set last element to include 'and'
+      array.join(', ')
+    else
+      nil
+    end
 end
