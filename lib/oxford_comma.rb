e92c073608016c7d8312578cdfc5b94389f19186
Lindsey Wells <lindsey.wells84@gmail.com>
2019-06-21 05:04:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,14 @@
 def oxford_comma(array)
-  
-  
-    if array.size == 1
+  if array.size == 1
       array[0]
-   elsif array.size == 2  
+  elsif array.size == 2  
      "#{array[0]} and #{array[1]}"
   else
      last = array.pop
     array.join(", ") << ", and #{last}"
 end
 end
+
+
+
+
