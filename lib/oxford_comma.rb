8cc2cc0d49b400aad94cf3fe4436275b205d0023
David Strauch <dmaster18@gmail.com>
2019-07-24 18:52:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,17 +2,19 @@ def oxford_comma(array)
   counter = 0
   new_string_element = ""
   new_string = ""
-  if array.length == 1 
+  case array.length
+  when 1 
     new_string = array.join
-  elsif array.length == 2 
+  when 2 
     new_string = array.join (" and ")
   else
     while counter <= array.length - 1
-      if counter == array.length - 2 
-        new_string_element = array[counter].to_s << ", and "
-        new_string = new_string << new_string_element
-      elsif counter == array.length - 1 
+      case counter
+      when array.length - 1 
         new_string_element = array[counter].to_s
+        new_string = new_string << new_string_element
+      when array.length - 2 
+        new_string_element = array[counter].to_s << ", and "
         new_string = new_string << new_string_element
       else
         new_string_element = array[counter].to_s << ", "
@@ -24,4 +26,8 @@ def oxford_comma(array)
   new_string
 end
 
+puts oxford_comma(["Pepperoni"])
+puts oxford_comma(["Pepperoni", "Sausage"])
 puts oxford_comma(["Pepperoni", "Sausage", "Meatball"])
+puts oxford_comma(["Pepperoni", "Sausage", "Meatball", "Anchovies"])
+puts oxford_comma(["Pepperoni", "Sausage", "Meatball", "Anchovies", "Extra Cheese"])
