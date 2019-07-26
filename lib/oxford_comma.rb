3899807c6946452d3c1c2feb893c39a8163d71d7
Isai Bismark Cortez <isai0505@gmail.com>
2019-07-26 19:41:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def oxford_comma(array)
-
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length == 3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  elsif array.length > 3
+    array[-1].insert(0,"and ")
+  end
+  array.join(", ")
 end
+
+#def oxford_comma(arrayy)
+
+#end
