7bc1621e1022fddc41ab96306c1689fe9aa1ff27
Blythe Pollard <blytheepollard@gmail.com>
2019-09-08 04:28:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-  array.join(",")
+  if array.length == 1
+   return array.join(",")
+  elsif array.length == 2
+   return array.join(" and ")
+  elsif array.length == 3
+    last = array.pop
+    return array.join(", ") + ", and " + last
+    
+  elsif array.length > 3
+    last = array.pop
+    return array.join(", ") + ", and " + last
+end
 end
