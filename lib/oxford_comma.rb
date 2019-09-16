a34df3ad51c61f5552f1c02418ced0cc76a739b4
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-09-16 18:44:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return "#{array[0]}"
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size == 3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    last_element = array.pop()
+    return array.join(", ") + ", and #{last_element}"
+  end
 end
+
+arraySample = [1,2,3,4,5]
+print arraySample.pop()
+print arraySample
