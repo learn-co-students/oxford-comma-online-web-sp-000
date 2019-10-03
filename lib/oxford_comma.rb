09be68b1655a01d4e66721d2fafae96a207eeae3
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-10-03 18:03:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+elements= array.length 
+case elements 
+when 1 
+  array.join 
+  when 2 
+    array.join(" and ")
+  else 
+    array[-1]= "and #{array[-1]}"
+    array.join(", ")
 end
+end 
