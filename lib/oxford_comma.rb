162f969405ef4846b3b5eb04753e8260979d793a
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-10-15 19:14:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,26 @@
-def oxford_comma(array)
 
+array = ["fiddleheads","okra","kohlrabi"]
+def oxford_comma(array)
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")    
 end
+oxford_comma(array)
+
+=begin
+array = ["fiddleheads","okra","kohlrabi"]
+def oxford_comma(array)
+  if array.length == 1
+    puts array.join
+  elsif array.length == 2
+    puts array.join(" and ")
+  else
+    last = array.pop
+    puts "#{array.join(", ")}, and #{last}"
+  end
+end
+oxford_comma(array)
+=end
