28434393e75d40d135db94debb488bbea9115b24
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-25 08:14:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+  if array.length == 1
+    return array.join
 
-end
+  elsif array.length == 2
+    return  array.join(" and ")
+
+
+  elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+  #  array.insert(-2, "and ")
+    return array.join(", ")
+  end
+  end
+  #  array(index-1).push( "and ")
+  # array[4].push("and ")
+  # array[4].join("and ")
+  #new_array =  array.join(", ")
