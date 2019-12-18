879cbc524a161a34f3a6e7b961fa2b0f17a75b43
James Bridges <masterbridges@gmail.com>
2019-12-18 22:28:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size == 2 
+    return array.join(" and ")
+  elsif array.size == 1
+    return array.join
+  elsif array.size == 3
+    newarray = array.pop
+    array = array.join(", ")
+    return "#{array}, and #{newarray}" 
+  elsif array.size > 3
+    newarray = array.pop
+    array = array.join(", ")
+    return "#{array}, and #{newarray}" 
+  end
 end
