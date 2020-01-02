2ef048cc5a5363a88458ed9f2d7ef7b0f991bcd8
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-02 19:17:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join
+  elsif array.length == 2 
+    array.join(" and ")
+  else   
+    last_ele = array.pop()
+    current_array = array.join(", ")
+    "#{current_array}, and #{last_ele}"
+  end 
 end
