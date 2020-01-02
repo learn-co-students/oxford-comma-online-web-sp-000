763dc0a11d88918a98a088d5452ccaf6086f8c5f
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-02 23:14:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  #using a case statment for the different possiblities
+  len = array.length
+  case len
+  when 0
+    ""
+  when 1 
+    array[0]
+  when 2 
+    array.join(" and ")
+  else 
+    array_back = array.pop
+    joined_array = array.join(", ")
+    joined_array << ", and " + array_back
+  end
 end
