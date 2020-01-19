ffd20970ab6181a908eda6a67fa72bab914675c3
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-19 18:27:36 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
 def oxford_comma(array)
+  if array.length == 2
+    x = array.join(" and ")
+    return x
+  elsif array.length == 1
+    return array[0]
+  elsif array.length == 3
+    arr_2 = []
+    array.each do |x|
+      arr_2 << x + ","
+    end
+    arr_2[2] = "and " + array[2]
+    return arr_2.join(" ")
+  elsif array.length > 3
+    arr_3 = []
+    array.each do |x|
+      arr_3 << x + ","
+    end
+    arr_3[-1] = "and " + array[-1]
+    return arr_3.join(" ")
+  end
 
 end
