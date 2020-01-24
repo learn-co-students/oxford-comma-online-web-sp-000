6c4244a9ed4e34bb34478daa700ba45c031444cc
Shane Geary <stg4462@comcast.net>
2020-01-24 19:20:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
 
-end
+  if(array.length == 1)
+    return array.join()
+  end 
+  if(array.length == 2)
+    return array.join(" and ")
+  end 
+  newStr = "" 
+  array.each_with_index do |ele, idx| 
+    if(idx == (array.length - 1))
+      newStr += "and #{ele}"
+    else 
+    newStr += "#{ele}, "
+    end 
+  end 
+  return newStr 
+end 
