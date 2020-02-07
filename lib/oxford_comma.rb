78f0c5bc5b25457623dbd7936bcd7aeb854c1ca5
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-07 23:04:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length > 1
+    array = array.insert(array.length-1, "and " + array.last)
+    array.pop
+  end
+  if array.length >= 3
+    array.join(", ")
+  else
+    array.join(" ")
+  end
 end
