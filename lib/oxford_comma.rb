ee89f0b2b0562e750605c27bac525447ab7ad132
Alvin Coulson <aaacoulson@gmail.com>
2020-02-19 02:18:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 2
+    array[-2] << " and "
+    array.join
+  elsif array.size > 2
+    array[-1].prepend "and "
+  	array.join(", ")
+  else
+    array.join
+  end
 end
