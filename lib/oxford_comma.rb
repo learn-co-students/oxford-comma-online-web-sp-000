cca768f05665a23b526f3dcaeaca83899552c80a
Jake Garcia <bballwiz4@gmail.com>
2020-02-22 16:37:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
 
+  if array.size < 2
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  elsif array.size > 2
+
+    oxford_string = ""
+    array.each_with_index do |string, i|
+      if i != array.size - 1
+        oxford_string << "#{string}, "
+        next
+      end
+      oxford_string << "and #{string}"
+    end
+    return oxford_string
+  end
+
 end
