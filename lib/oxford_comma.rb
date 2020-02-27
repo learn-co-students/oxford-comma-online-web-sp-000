f7bcc2f7bd44330b5883faefaac92969e903d44f
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-27 04:11:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
-def oxford_comma(array)
-
+def oxford_comma(arr)
+  if arr.size == 1
+    return arr[0]
+  elsif arr.size == 2
+    return arr.join(" and ")
+  else
+    return arr[0..-2].join(', ') + ", and " + arr[-1]
+  end
 end
