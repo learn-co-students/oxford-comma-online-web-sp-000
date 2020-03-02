8ed8efcb4bbaf67644aa8a8ed8f2ea8a832a267a
C. Leow <chienleow1@gmail.com>
2020-03-02 19:56:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,13 @@
+## require 'pry'
+
 def oxford_comma(array)
+ ## binding.pry
   if array.size == 1
       return "#{array[0]}"
     elsif array.size == 2
-      return array.join(" and ")
+      return array.join("and")
     else array.size >= 3
-      #{array[-1]}.insert(-2," and ")
+      array.insert(-2,"and")
       return array.join(", ")
   end
 end
