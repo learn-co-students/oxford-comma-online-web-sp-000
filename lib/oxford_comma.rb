7cdf4308aa0f322b14e3f32522378e548cd2a89d
C. Leow <chienleow1@gmail.com>
2020-03-02 16:28:36 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,10 @@
 def oxford_comma(array)
-  case array
-    when array.size == 1
+  if array.size == 1
       return "#{array[0]}"
-    when array.size == 2
+    elsif array.size == 2
       return array.join(" and ")
     else array.size >= 3
-      array.insert(-2," and ")
+      string.insert(-2," and ")
       return array.join(", ")
   end
 end
