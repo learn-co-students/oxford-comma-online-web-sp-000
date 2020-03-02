558d41b3e820b30687e53642dd0321efe694fc91
C. Leow <chienleow1@gmail.com>
2020-03-02 05:20:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,14 @@
 def oxford_comma(array)
-  when array.size == [1]
-  return array.join
-  array.join(" and ")
+  case array
+    when array.size == [1]
+      return "#{array[0]}"
+    when array.size == [2]
+      return array.join(" and ")
+    when array.size >= [3]
+      new_array << 
+      return new_array
   end
 end
+
+
+## CONVERT ARRAY TO STRING
