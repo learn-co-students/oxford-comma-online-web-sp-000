6ff0458d45efba678c3c5f641f764a362d9ebf1a
C. Leow <chienleow1@gmail.com>
2020-03-03 01:05:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,9 +5,9 @@ def oxford_comma(array)
   if array.size == 1
       return "#{array[0]}"
     elsif array.size == 2
-      return array.join("and")
+      return array.join(" and ")
     else array.size >= 3
-      array.insert(-2,"and")
+      array.insert(-2," and ")
       return array.join(", ")
   end
 end
