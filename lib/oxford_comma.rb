2824371d2be7b2d87203abb4ef81d1f7e349f32f
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-20 22:49:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
 
+  new_arr = array.collect {|el|
+
+      if array.length == 1 || array.index(el) == array.length-1
+        el = el
+      elsif array.index(el) == array.length - 2
+        array.length != 2 ? el += ", and " : el += " and "
+      else
+        el += ", "
+      end
+  }
+
+  new_arr.join
+
 end
