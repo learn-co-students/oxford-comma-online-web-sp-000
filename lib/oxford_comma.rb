b5cc34bd7832245cfb70e7117378905eb5824a4a
Adam Spencer <aspencer0919@gmail.com>
2020-04-01 19:55:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return "#{array[0]}"
+  elsif array.size == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+    array[-1].prepend "and "
+  array.join(", ")
+#    #counter == 0
+#    while words < array.size
+#      array.each do |x|
+#        return "#{x}, "
+#        words += 1
+#      end
+#    return last(array)
+#    end
+  end
 end
