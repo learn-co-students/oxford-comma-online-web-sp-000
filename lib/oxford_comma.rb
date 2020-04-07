631f4e1334e410cfce9ba07c0978a870269db063
Joseph Kao <josephtkao@gmail.com>
2020-04-07 18:56:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  i = 0
+  string = ""
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    while i <= array.length - 2
+      string << array[i]
+      string << ", "
+      i += 1
+    end
+  end
+  string << "and #{array[-1]}"
 end
