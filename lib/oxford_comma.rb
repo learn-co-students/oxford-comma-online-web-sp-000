19089804d8a7e32e6e733aa52a190ce72058ff30
Gabriel Kutik <gabrielkutik@yahoo.com>
2020-04-09 19:55:35 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+if array.length == 1
+    array.join
+elsif array.length ==2
+    array.join(" and ")
+else 
+    last = array.pop()
+    list = array.join(", ")
+    answer = "#{list}, and #{last}"
+end
 end
