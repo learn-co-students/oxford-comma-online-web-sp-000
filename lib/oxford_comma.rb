1ca7968234da7f207dbd63472b255fca6a484779
Elliot Blanchard <public@elliotblanchard.com>
2020-04-11 17:19:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  case array.length
+    when 1
+      array.join
+    when 2
+      array.join(" and ")
+    else
+      last_element = array.pop
+      result_string = array.join(", ")
+      result_string = result_string << ", and "
+      result_string = result_string << last_element
+  end
 end
