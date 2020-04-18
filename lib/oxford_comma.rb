3d09d2835fb332308486f04220bc868290331a96
Jung Lee <lee.jung.s95@gmail.com>
2020-04-18 06:42:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,3 +9,12 @@ def oxford_comma(array)
     [array[0..-2].join(", "), array.last].join(", and ")
   end
 end
+
+def oxford_comma(array)
+if array.length == 2
+  return "#{array[0]} and #{array[1]}"
+elsif 2 < array.length
+  array[-1].insert(0, "and ")
+end
+array.join(", ")
+end
