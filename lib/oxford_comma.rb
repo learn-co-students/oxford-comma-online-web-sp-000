4ccf463668177938bc5d58e35604d6aa52f40cff
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2020-04-18 21:50:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array.join(' and ')
+  elsif array.length >= 3
+    element = array[-1]
+    array[-1] = 'and ' + element
+    array.join(', ')
+  else
+    array[0]
+  end
 end
