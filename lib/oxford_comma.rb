4a3bc241399399ba3107fd1ca523da3d5da9c079
Jake G <jake098877@gmail.com>
2020-04-27 23:04:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,36 @@
 def oxford_comma(array)
+  case array.length
+  when 1
+    array.join
+  when 2
+    array.join(" and ")
+  when 3
+    array[2] = "and " + array[2]
+    array.join(", ")
+  else
+    n = array.length - 1
+    array[n] = "and " + array[n]
+    array.join(", ")
+  end
 
+  # l = array.length
+  # if l = 1
+  #   array.join
+  # elsif l = 2
+  #   array.join(" and ")
+  # elsif l = 3
+  #   array[2] = "and " + array[2]
+  #   array.join(", ")
+  # else
+  #   array.length > 3
+  #   n = array.length - 1
+  #   array[n] = "and " + array[n]
+  #   array.join(", ")
+  # end
+#
+#   array.to_a
+#   array.split(", and")
+#   array.join("and")
+#   array.select {|i| i > 3
+#     array.to_a}
 end
