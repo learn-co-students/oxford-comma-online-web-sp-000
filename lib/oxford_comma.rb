18b67f836a227e4bb40f77db77f357d88cbb154a
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-19 01:54:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,10 @@ def oxford_comma(array)
 
 if array.length == 1
   array.join('')
-elsif array.size == 2
-  return array.join(" and ")
+elsif array.length == 2
+  array.join(" and ")
 else
-  return array[0..-2].join(', ') + ", and " + array[-1]
+  array[-1] = "and #{array[-1]}"
+      array.join(', ')
   end
 end
