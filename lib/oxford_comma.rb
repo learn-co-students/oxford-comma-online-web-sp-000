8ff5f74cd64592b79bda345b0a1577b552ca1fc1
Aliya Merali <aliya.merali@flatironschool.com>
2020-05-25 16:01:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length > 2
+    final = array.pop
+    array << "and #{final}"
+    return array.join(", ")
+  else
+    return array.join(" and ")
+  end
 end
+#
+# array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
+# oxford_comma(array)
