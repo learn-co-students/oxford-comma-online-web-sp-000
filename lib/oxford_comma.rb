8e54b00e149b2a318d5d7e3cfd14952bdf0b652e
Nick Lanese <nlanese21@gmail.com>
2020-05-29 19:42:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+  i = 0
+  return_array = []
+  len = array.length() - 1
+  while (i != len)
+    return_array << array[i]
+    if (i + 1) == len
+      if (len == 1)
+        return_array << " and "
+      else
+        return_array << ", and "
+      end
+    else
+      return_array << ", "
+    end
+    i = i + 1
+  end
+  return_array << array[i]
+  return return_array.join
 end
