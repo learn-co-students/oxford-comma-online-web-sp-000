02593219831bb364d198ef86b5819c2338f1185c
Carrie Jones <c.jessica.jones@gmail.com>
2020-06-07 15:16:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  result = ""
+  counter = array.length
+  case counter
+  when 1
+    result << array[0]
+  when 2
+    result = array[0] + " and " + array[1]
+  else
+    counter = array.length - 2
+    result = array[0..counter].join(", ")
+    result = result + ", and " + array[counter + 1]
+  end 
+  return result
 end
