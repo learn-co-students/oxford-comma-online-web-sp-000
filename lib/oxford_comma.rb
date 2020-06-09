6e78a6d30ac18c951d45e1dd3d525e7a134c39f1
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-09 13:32:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.length == 2 
+    array.join (" and ")
+  elsif array.length >= 3
+    final_string = ""
+    array.each do |index|
+      if index == array.last
+        final_string << "and "
+        final_string << index
+      else
+        final_string << index
+        final_string << ", "
+      end
+    end
+    return final_string
+  elsif array.length == 1
+    return array.join()
+  end
 end
