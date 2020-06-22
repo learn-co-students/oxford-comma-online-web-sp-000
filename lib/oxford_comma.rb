8300b07024c52d2b8a957debc8eb1763a66baa91
An Pham <anpham148@gmail.com>
2020-06-22 11:55:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    str = array.join
+  elsif array.count == 2
+    str = array.join(" and ")
+  else
+    new_arr = ["and", array[-1]]
+    new_var = new_arr.join(" ")
+    array.delete(array[-1])
+    new_arr1 = array.push(new_var)
+    str = new_arr1.join(", ")
+  end
+  str
 end
