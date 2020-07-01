db41c617f0ae79433058a9beb66f418381a292e1
Adi Skop <adiskop@gmail.com>
2020-07-01 13:51:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+if array.length==1
+  array.join
 
+elsif array.length==2
+  array.join(' and ')
+else
+array[0..-2].join(", ") + ", and " + array[-1]
+  end
 end
+
+
+
+#    a[-1]    # last item in the array
+#    a[-2:]   # last two items in the array
+#    a[:-2]   # everything except the last two items
