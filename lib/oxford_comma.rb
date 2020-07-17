7537555eccfe2f29d0c138d82f1446480b473b92
Ian Mayton <ian.mayton@gmail.com>
2020-07-17 18:27:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
-
+  if array.join == array[0]
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    counter = 0
+    string = ''
+    array.each do |item|
+      counter += 1
+      if counter == array.length - 1
+        string << item + ", and "
+      elsif counter == array.length
+        string << item
+      else
+        string << item + ", "
+      end
+    end
+    string
+  end
 end
