c21ff11456059ef3a8357460ff2a12b807537956
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-19 17:53:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length > 2
+    last_word = array.pop
+    array[0..array.length-1].join(", ") + ", and " + last_word
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array[0]
+  end
 end
