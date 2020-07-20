df25d4ade5c9cd755e615778fdbcebbc98db7409
jrog20 <jaimerogstad@gmail.com>
2020-07-20 12:57:45 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
+# Takes in an array of string elements and converts it into a string
 
+def oxford_comma(array)
+  if array.length <= 2
+    # if array has only one element, do nothing
+    # if array has two elements, join with "and"
+    array.join(" and ")
+  else
+    array[0..-2].join(", ") + "," + " and " + array[-1]
+    # add the new element ("and") in the second to last position
+    #array.insert(-2, "and")
+    # convert array to string
+    #array.join(", ")
+    # remove last comma
+    # hmmmm...
+  end
 end
