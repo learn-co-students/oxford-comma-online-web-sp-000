c08872f4445a42a5c0d7459574b3dd0a5e2e18ed
Ezenwa Onukwue <eonukwue@gmail.com>
2020-07-31 03:34:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
+require 'pry'
 def oxford_comma(array)
+
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    value = array.pop
+    result = array.join(", ") + ", and " + value
+  end
 
 end
