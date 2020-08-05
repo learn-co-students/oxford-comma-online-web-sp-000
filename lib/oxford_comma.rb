f9dfe7828ac4ccf90a0e55fb3e6f32f48883b9b7
McKayla Lankau <lankaukk@yahoo.com>
2020-08-05 01:50:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.count ==  1
+    return array.join(", ")
+  elsif array.count == 2 
+    return array.join(" and ")
+  elsif array.count > 2 
+    last = array.size 
+    array[-1].insert(0, "and ")
+    return array.join(", ") 
+  end
 end
+
+
+
+# array[second_to_last] << " and"
