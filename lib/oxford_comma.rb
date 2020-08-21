4935a78f5899c72c2d376f159895b53291275abf
Javone <javosmith12@gmail.com>
2020-08-21 17:01:45 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
+require 'pry'
 
-end
+def oxford_comma(array)
+    if array.length == 2
+      array[-1].insert(0, "and ")
+      return array.join(" ")
+    elsif array.length > 2
+      array[-1].insert(0, "and ")
+    end
+    array.join(", ")    
+  end
