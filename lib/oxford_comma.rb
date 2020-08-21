f3bf2e2346d6f4d1554a1f5831c038dd552a3fc3
John Stassen <john.stassen@gmail.com>
2020-08-21 05:13:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  count = array.count
+  case count
+    when 1
+      return array.join
+    when 2
+      return array.join(" and ")
+    else
+      last = array.pop
+      string = array.join(", ")
+      string << ", and "
+      string << last
+      return string
+  end
 end
