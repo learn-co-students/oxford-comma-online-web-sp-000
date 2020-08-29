9eb981a7c490b90355abef67dbf043db373eed8b
Allison MacQueen <allimacq@gmail.com>
2020-08-29 00:00:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length() < 3
+    new = array.join(" and ")
+    return new
+  elsif array.length > 2 && array.length < 100
+    new = array.insert(-2, " and")
+    final = new.join(", ")
+    final[/\W\sand\W/] = " and"
+    return final
+  end
 end
