52480d7bb685cc0504cb323c326d906ac2ba8e72
Catherine Gloria <catherinegloria@gmail.com>
2020-09-04 10:31:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    string = ""
+    end_of_string = "and " + array.pop
+    array.each do |element|
+        string << element + ", "
+    end
+    string + end_of_string
+  end
 end
