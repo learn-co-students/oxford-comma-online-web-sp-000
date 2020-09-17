7cee085c4d7fce3dd6a3eb6be7c802fe6f416c46
fut0k0 <cccabell@icloud.com>
2020-09-17 14:00:58 -0700

    Passing lab

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+	if array.size <= 1
+		array[0]
+	elsif array.size == 2
+		array.join(" and ")
+	else
+		array[-1].prepend("and ")
+		array.join(", ")
+	end
 end
