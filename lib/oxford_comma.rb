11c681abfc67f3f74bd38b6debb1e149dbdd0930
Carolyn Manning <carolyn.manning3@gmail.com>
2020-09-23 01:39:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  elsif array.count > 2
+    last_element = array.last
+    array.pop
+    string = array.join(", " )
+    string << ", and #{last_element}"
+  end
 end
