0cbf977cafbfd45e0ace9a72821a5135bb34047f
Carolyn Manning <carolyn.manning3@gmail.com>
2020-09-23 01:51:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,5 @@
 def oxford_comma(array)
-  if array.count == 1
-    array.join
-  elsif array.count == 2
+  if array.count <= 2
     array.join(" and ")
   elsif array.count > 2
     last_element = array.pop
