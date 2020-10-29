e52df0e3be0f285bf689a08d9875b954201c0ae4
Nathan Glick <nateleighglick@hotmail.com>
2020-10-29 07:56:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+ if array.length == 1 
+   array.join
+ elsif array.length == 2
+   array.join(" and ")
+ else 
+   oxford_array = array
+   oxford_array[oxford_array.length - 1] = "and #{oxford_array[oxford_array.length - 1]}"
+   oxford_array = array.join(", ") 
+ end
 end
