a6426119c814bcd0a86421e1629fda76aaf5f39d
Jose Melero Martinez <jmelero08@gmail.com>
2020-10-29 01:46:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join
+  elsif array.length == 2  
+    array.join(" and ")
+  else
+    array.each.collect do |value|
+      if value == array[-1]
+        array[-1] = "and " << value
+      end
+  end
+    array.join(", ")
+  end
 end
