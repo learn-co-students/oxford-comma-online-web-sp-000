8f2c76bb5c40e27a1651b7ee866a08251a575ea5
Benjamin Cheng <bencheng200@gmail.com>
2020-11-11 19:35:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  arr = []
+  count = 0
+  if array.length == 2
+    arr.push(array[0]+ " and " + array[1])
+    return arr.join
+  end
+  while count < array.length - 1
+    arr.push(array[count]+", ")
+    count += 1
+  end
+  if count == 0
+    arr.push(array[count])
+    arr.join
+  else
+    arr.push("and "+array[count])
+    arr.join
+  end
 end
