3a3cc09588eb2b32948d8eef7c59cc62f2f8dc33
Ben Hayes <b.t.hayes@comcast.net>
2020-12-11 00:06:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.join
+    elsif array.size == 2 
+    return array.join(" and ")
+    elsif array.size >= 3
+    last_word = array.pop
+    beginning = array.join(", ")
+    final = beginning << ", and #{last_word}"
+  end
 end
