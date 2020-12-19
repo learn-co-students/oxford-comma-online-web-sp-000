842a805c55abd7e9a1d550d95d0e45486a648224
JC <bonassin@gmail.com>
2020-12-19 00:55:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,5 @@
 def oxford_comma(array)
-  if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
-    array[-1].insert(0, "and ")
-  end
-  array.join(", ")
+  return array.join(' and ') if array.size < 3
+  array[-1] = "and " + array[-1]
+  array.join(', ')
 end
