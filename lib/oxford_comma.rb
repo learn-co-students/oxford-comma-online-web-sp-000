4c081929a450fee74c3de5bef68a4d0bd06f5e5a
Jeff Bai <j.bai@wustl.edu>
2021-01-04 05:49:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
-def oxford_comma(array)
-
+def oxford_comma(sentence)
+  if sentence.length == 2
+    return "#{sentence[0]} and #{sentence[1]}"
+  elsif sentence.length > 2
+    sentence[-1] = "and #{sentence[-1]}"
+  end
+  sentence.join(", ")
 end
