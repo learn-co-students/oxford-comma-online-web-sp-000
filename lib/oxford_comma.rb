e66274df3e0ab0c8cfa166d393c80cb5c9f27738
Chris Baum <christopher.baum@valpo.edu>
2021-01-12 00:57:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+  array_last_element = Array.new
+  case
+    when array.count == 1
+      array.join
+    when array.count == 2
+      array.join(" and ")
+    when array.count > 2
+      num_of_elements = array.count #count number of elements in array
+      last_index_count = num_of_elements - 1 #get index # of final element
+      array_last_element = array #copy array
+      array_last_element[last_index_count] = "and " + array.last #change last element to add "and "
+      array_last_element.join(", ") #add commas and convert to string
+  end
 end
