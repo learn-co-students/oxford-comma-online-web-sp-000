81c5f33013d6ba0b316163345bd7c8e487104209
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-22 18:57:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  last = array.pop()
+  if array.length == 0
+    last
+  elsif array.length == 1
+    array[0] + " and #{last}"
+  else
+    array.join(", ") + ", and #{last}"
+  end
 end
