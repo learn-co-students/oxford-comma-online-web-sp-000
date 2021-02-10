544476fc996ad7a401c11a0efefc37196b82ed59
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-10 00:03:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    else
+        # put a comma after each, but add "and" before the last word
+        array.each.collect do |value|
+            if value == array[-1]
+                array[-1] = "and " << value
+            end
+        end
+        array.join(", ")
+    end
 end
