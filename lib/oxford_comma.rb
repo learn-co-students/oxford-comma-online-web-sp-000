af299efb426b3203a0569cbe95c3ff3e2ae305fd
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-03-28 20:01:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   elsif array.length > 2
-    last_element = array.pop
-    array[0..-1].join(", ") << ", and #{last_element}"
+    last_element = array[-1]
+    array[0..-2].join(", ") << ", and #{last_element}"
   else
     array.join
   end
