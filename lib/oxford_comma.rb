e106a3f3dfef2e152944ce95d0f6d454c054386a
Christopher Black <christopher.black.2209@googlemail.com>
2021-03-31 23:45:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,5 @@
 def oxford_comma(array)
     array.join(" and ")
 end
+
+#what about iterating through the array and adding commas into the array before converting, and somwhow knowing when the last variable is so adding an and before it?
