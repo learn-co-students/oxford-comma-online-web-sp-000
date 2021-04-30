38ddf158dc7535b1291ccef0356705f05af5fbe7
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-04-30 02:11:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+ if array.length >= 3
+  array[-1].prepend("and ")
+array.join(", ")
+elsif array.length == 2
+  array[-1].prepend("and ")
+   array.join(" ")
+else
+  array.join
+end
 end
