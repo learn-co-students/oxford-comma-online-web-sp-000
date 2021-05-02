495047ad851420c13d5fdb214dc7d38fa8581004
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-02 16:35:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  if array.length == 1 
+    array.pop
+  elsif array.length == 2 
+     array.join(" and ")
+  elsif array.length >= 3 
+     last_element = array.pop
+     new_last_element = "and #{last_element}" 
+     array.push(new_last_elment)
+     array.join(", ")
+  end
 end
+
+
