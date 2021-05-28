c81036dbd8230f2a6d79a980bc33d5c5c69d1319
James Tillman <james@itme-music.com>
2021-05-28 23:30:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+  case array.size
+  when 1
+    array.join
+  when 2
+    array.join(' and ')
+  else
+    give_it_the_comma = array
+    give_it_the_comma[-1] = "and #{give_it_the_comma[-1]}"
+    give_it_the_comma.join(", ")
+  end
 
 end
