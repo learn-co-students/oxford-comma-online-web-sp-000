e2b391edf0c1d7ae0da5c3f07ae3de0a35bef3b9
WildBeanOnIt <julioaguilera337@gmail.com>
2021-06-07 20:45:33 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,16 @@
 require 'pry'
 
 def oxford_comma(array)
-    array.join(" and ")
-    array.spit(",")
+    if array.length == 1
+        return array.join
+    end
+
+
+
+    if array.length == 2
+    return array.join(" and ")
+    end
+    array[-1] = "and " << array[-1] 
+    array.join(", ")
     #binding.pry
 end
